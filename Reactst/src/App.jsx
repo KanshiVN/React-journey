bc946b2688bde9df6d6cb4e1eb4a7193d88f63bd
kanshi <kanshinale332@gmal.com>
2024-09-17 03:27:56 +0530

    card

--- Reactst/src/App.jsx
+++ Reactst/src/App.jsx
@@ -1,36 +1,27 @@
-import React, { useState } from "react";
-import Songs2 from "./components/Songs2";
-import Navbar from "./components/Navbar";
+import React, { useRef, useState } from "react";
 
-function App() {
-    const data = [
-      {image:"https://images.unsplash.com/photo-1519699047748-de8e457a634e?q=80&w=1180&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",songName:"Bella", artistName:"Jessica",addToFavourite:false},
-      {image:"https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",songName:"Blue Sky", artistName:"Natasha",addToFavourite:false},
-      {image:"https://images.unsplash.com/photo-1726164959171-291645df9ed9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",songName:"Empty", artistName:"Kanshi",addToFavourite:true},
-      {image:"https://images.unsplash.com/photo-1472396961693-142e6e269027?q=80&w=1904&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",songName:"Mountain", artistName:"Tim Stief",addToFavourite:false},
-    ]
 
-    const [songData,setData] = useState(data);
-    const clickHandler =(index)=>{
-      setData((prev)=>{
-      return  prev.map((item,itemIndex)=>{
-        if(index === itemIndex ) return{...item, addToFavourite : !item.addToFavourite}
-        return item;
-      })
-      })
-    }
+
+
+function App(){
+
+  const name = useRef(null);
+  const age = useRef(null);
+  const submitData =(data)=>{
+    data.preventDefault();
+      console.log(name.current.value)
+      console.log(age.current.value)
+  }
+
 
   return (
+
     <>
-      <div className="w-full h-screen bg-slate-300"> 
-         <Navbar data={songData}/>
-         <div className="px-20 mt-32 flex gap-10 flex-wrap">
-          {songData.map((item,key)=>{
-            return <Songs2 data = {item} key={key} index={key} clickHandler = {clickHandler}/>
-          })
-        }
-         </div>
-      </div>
+     <form action="" className="mt-20 ml-20"  onSubmit={submitData}>
+      <input type="text" ref={name} placeholder="name" className="border-red-200"/>
+      <input type="text" ref={age} placeholder ="age" />
+      <input type="submit"/>
+     </form>
     </>
   );
 }
